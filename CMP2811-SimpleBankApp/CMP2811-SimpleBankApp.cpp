ca326c8bef7978da28dd49d35f9278b38198adbd
Dylan <158075286+d-hmlton@users.noreply.github.com>
2024-12-05 12:55:05 +0000

    Adding driver program code

--- CMP2811-SimpleBankApp/CMP2811-SimpleBankApp.cpp
+++ CMP2811-SimpleBankApp/CMP2811-SimpleBankApp.cpp
@@ -1,11 +1,91 @@
-// CMP2811-SimpleBankApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* ------------------------------------------------------
+CMP2811: Applied Programming Paradigms
+Driver program for "LincBank" Assessment Item 2
+Semester A 2024
 
+During marking, we will use the exact same notation
+as provided in the brief, so make sure you follow that guideline.
+
+You should avoid removing parts of this driver program, but
+add to it the necessary code to produce your implementation.
+
+Good luck!
+------------------------------------------------------ */
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::vector <std::string> parameters;
+	std::string userCommand;
+	// you may also want to store a collection of opened accounts here
+
+	std::cout << "~~~ Welcome to LincBank! ~~~" << std::endl;
+
+	while (userCommand != "exit")
+	{
+		parameters.clear(); // clear ready for next command
+		std::cout << std::endl << ">>> ";
+
+		std::getline(std::cin, userCommand);
+		char* cstr = new char[userCommand.length() + 1];
+		strcpy(cstr, userCommand.c_str());
+
+		char* token;
+		token = strtok(cstr, " ");
+
+		while (token != NULL)
+		{
+			parameters.push_back(token);
+			token = strtok(NULL, " ");
+		}
+
+		// Define all commands as per the brief
+		std::string command = parameters[0];
+
+		if (command.compare("options") == 0)
+		{
+			// display the various commands to the user
+		}
+		else if (command.compare("open") == 0)
+		{
+			// allow a user to open an account
+			// e.g., Account* a = new Savings(...);
+		}
+		else if (command.compare("view") == 0)
+		{
+			// display an account according to an index (starting from 1)
+			// alternatively, display all accounts if no index is provided
+		}
+		else if (command.compare("withdraw") == 0)
+		{
+			// allow user to withdraw funds from an account
+		}
+		else if (command.compare("deposit") == 0)
+		{
+			// allow user to deposit funds into an account
+		}
+		else if (command.compare("transfer") == 0)
+		{
+			// allow user to transfer funds between accounts
+			// i.e., a withdrawal followed by a deposit!
+		}
+		else if (command.compare("project") == 0)
+		{
+			// compute compound interest t years into the future
+		}
+		//else if (command.compare("search"))
+		//{
+		//	allow users to search their account history for a transaction
+		//  (this is a stretch task)
+		//}
+
+	}
+
+	std::cout << "Press any key to quit...";
+	std::getchar();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
